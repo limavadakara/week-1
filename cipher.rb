9c7a6eecaff468952a3d087ada7a02da145a296d
Student <student@Admins-MacBook-Pro.local>
2019-02-05 12:30:59 +0000

    Add initial code for cipher

---
+++ cipher.rb
@@ -0,0 +1,7 @@
+def encode(plaintext, key)
+  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  ciphertext_chars = plaintext.chars.map do |char|
+    (65 + cipher.find_index(char)).chr
+  end
+  ciphertext_chars.join
+end
