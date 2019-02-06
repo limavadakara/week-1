99b5a521b1d9f55400575946ae7d8b0634b21e86
Lima Vadakara <lima.vadakara@gmail.com>
2019-02-06 11:38:15 +0000

    first debug excercise

--- cipher.rb
+++ cipher.rb
@@ -1,7 +1,17 @@
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
   ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
   ciphertext_chars.join
 end
+
+def decode(ciphertext, key)
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  plaintext_chars = ciphertext.chars.map do |char|
+    cipher[65 -char.ord]
+  end
+  plaintext_chars.join
+end
+puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
